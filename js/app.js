f619596dc2dee20c7b2113d88d0c37edcbca7528
alirajuansills <alisill@mail.regent.edu>
2020-08-31 08:22:01 -0400

    baguetteBox bug 2

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 baguetteBox.run(".gallery");
 
-// const search = new Filter("search", "data-caption");
+const search = new Filter("search", "data-caption");
 
-function myFunction() {
-  var input, filter, a, alt, i, gallery, txtValue;
-  input = document.getElementById("search");
-  filter = input.nodeValue.toUpperCase();
-  gallery = document.getElementsByClassName("gallery");
-  a = a.getElemntsByTagName("a");
+// function myFunction() {
+//   var input, filter, a, alt, i, gallery, txtValue;
+//   input = document.getElementById("search");
+//   filter = input.nodeValue.toUpperCase();
+//   gallery = document.getElementsByClassName("gallery");
+//   a = a.getElemntsByTagName("a");
 
-  for (i = 0; i < a.length; i++) {
-    alt = a[i].getElemntsByTagName("alt")[0];
-    txtValue = alt.textContent || alt.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
-  }
-}
+//   for (i = 0; i < a.length; i++) {
+//     alt = a[i].getElemntsByTagName("alt")[0];
+//     txtValue = alt.textContent || alt.innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       a[i].style.display = "";
+//     } else {
+//       a[i].style.display = "none";
+//     }
+//   }
+// }
